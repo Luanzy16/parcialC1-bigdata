e83f83e7aa47e562a4077d44d63875df4216c1ed
Luis Sánchez <luanzy1610@gmail.com>
2025-03-12 14:25:19 +0000

    arreglando errores

--- parser/test/test_parcial_p.py
+++ parser/test/test_parcial_p.py
@@ -2,32 +2,6 @@ from unittest.mock import patch, MagicMock
 import parser.parcial_p as parcial_p
 
 
-@patch("parser.parcial_p.s3_client.get_object")
-def test_app(mock_get_object):
-    """Prueba para verificar la función principal cuando el HTML es válido."""
-    test_html = """
-    <html>
-        <body>
-            <div class="listing-card__information">
-                <span class="listing-card__location">Chapinero</span>
-                <span class="price">$500,000,000</span>
-                <span class="bedrooms">3 Habitaciones</span>
-                <span class="bathrooms">2 Baños</span>
-                <span class="area">80 m²</span>
-            </div>
-        </body>
-    </html>
-    """
-
-    mock_get_object.return_value = {
-        "Body": MagicMock(read=lambda: test_html.encode("utf-8"))
-    }
-
-    response = parcial_p.app({}, {})
-    assert response["statusCode"] == 200
-    assert "Archivo CSV guardado correctamente." in response["body"]
-
-
 @patch(
     "parser.parcial_p.s3_client.put_object",
     side_effect=Exception("S3 no disponible")
